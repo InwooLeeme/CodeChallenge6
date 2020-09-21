e5fc4b58ae2d9ccd684ab935647b03d7edbc6549
ItsmeLee <arsmagica90@gmail.com>
2020-09-21 13:07:33 +0900

    Add text in pug files
    Added the text in pug files
    Created new pug file called home for render home page.
    And include footer in index.pug file for take copyright icons in all template page.
    Created new javascript file called middleware for meet the conditions.

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,4 +1,4 @@
-export const home = (req, res) => res.render("index");
+export const home = (req, res) => res.render("home");
 export const Login = (req, res) => res.send("login");
 export const photos = (req, res) => res.send("photos");
 export const profile = (req, res) => res.send("profile");
